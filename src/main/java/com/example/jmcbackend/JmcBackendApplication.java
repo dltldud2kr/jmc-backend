69558ce5783cdab59aa067c61501c9701b9418b9
explorer-cat <sqlstyle@naver.com>
2023-02-17 23:44:54 +0900

    Initial commit

---
+++ src/main/java/com/example/jmcbackend/JmcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.jmcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmcBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmcBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jmcbackend/JmcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmcBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
