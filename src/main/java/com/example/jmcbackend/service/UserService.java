7103883605b5372ce170b522124283ce8991770a
dltldud2kr <dltldud2kr@gmail.com>
2023-02-25 22:18:04 +0900

    [2023-02-25] 토큰 발행 진행중

---
+++ src/main/java/com/example/jmcbackend/service/UserService.java
@@ -0,0 +1,58 @@
+package com.example.jmcbackend.service;
+
+import com.example.jmcbackend.domain.User;
+import com.example.jmcbackend.exception.AppException;
+import com.example.jmcbackend.exception.ErrorCode;
+import com.example.jmcbackend.repository.UserRepository;
+import com.example.jmcbackend.utils.JwtTokenUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
+
+    private String key = "1q2w3e4r";
+    private Long expiredTimeMs = 1000 * 60 * 60l;
+
+    public String join(String userName, String password) {
+
+        // userName 중복 check
+        userRepository.findByUserName(userName)
+                .ifPresent(user -> {
+                    throw new AppException(ErrorCode.USERNAME_DUPLICATED , userName + "는 이미 있습니다.");
+//                    throw new RuntimeException(userName + "는 이미 있습니다.");
+                });
+
+        // 저장
+        User user = User.builder()
+                .userName(userName)
+                .password(encoder.encode(password))
+                .build();
+        userRepository.save(user);
+
+        return "SUCCESS";
+    }
+
+    public String login(String userName, String password){
+        // userName 없음
+        User selectedUser = userRepository.findByUserName(userName)
+                .orElseThrow(() ->new AppException(ErrorCode.USERNAME_NOT_FOUND, userName + "이 없습니다."));
+
+        // password 틀림
+        if(!encoder.matches(password, selectedUser.getPassword())) {
+            throw new AppException(ErrorCode.INVALID_PASSWORD, "패스워드를 잘못 입력했습니다.");
+        }
+
+        String token = JwtTokenUtil.createToken(selectedUser.getUserName(), key, expiredTimeMs);
+
+        // Exception 안 났을 때 토큰 발행
+
+        return token;
+    }
+}
---
+++ src/main/java/com/example/jmcbackend/utils/JwtTokenUtil.java
@@ -0,0 +1,24 @@
+package com.example.jmcbackend.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Builder;
+
+import java.util.Date;
+
+
+public class JwtTokenUtil {
+    public static String createToken(String userName, String key, long expireTimeMs) {
+        Claims claims = Jwts.claims(); // 일종의 map
+        claims.put("userName", userName);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))  // 만든 날짜
+                .setExpiration(new Date(System.currentTimeMillis() + expireTimeMs)) // 끝나는 날짜
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact()
+                ;
+    }
+}
