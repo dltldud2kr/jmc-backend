5ba84bc1a25abade86de9aedf1cdba2f132b4217
dltldud2kr <dltldud2kr@gmail.com>
2023-02-25 22:08:08 +0900

    [2023-02-25] 토큰 발행 진행중

--- src/main/java/com/example/jmcbackend/JmcBackendApplication.java
+++ src/main/java/com/example/jmcbackend/JmcBackendApplication.java
@@ -1,13 +1,38 @@
 package com.example.jmcbackend;
 
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class JmcBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JmcBackendApplication.class, args);
+
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject;
+        String jsonTest = "{\"id\": \"1\", \"name\":\"jjang-ggae\", \"thumbnail\":\"jpg\", \"score\":\"4\", \"location\":\"chungsuro\"}";
+
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(jsonTest);
+        } catch(ParseException e){
+            throw new RuntimeException(e);
+        }
+
+        Map<String, Object> resultMap = new HashMap<>();
+
+        System.out.println("id값 파싱결과" + jsonObject.get("id"));
+        System.out.println("id값 파싱결과" + jsonObject.get("name"));
+
+//
+
     }
 
 }
--- src/main/java/com/example/jmcbackend/test1.java
+++
@@ -1,9 +0,0 @@
-package com.example.jmcbackend;
-
-public class test1 {
-    public static void main(String[] args) {
-        System.out.println("hi");
-
-    }
-
-}
