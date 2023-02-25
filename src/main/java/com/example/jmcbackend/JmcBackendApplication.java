a70e6434f97f797dd1885d9aa15781458f30762f
dltldud2kr <dltldud2kr@gmail.com>
2023-02-25 22:09:46 +0900

    [2023-02-25] 토큰 발행 진행중

--- src/main/java/com/example/jmcbackend/JmcBackendApplication.java
+++ src/main/java/com/example/jmcbackend/JmcBackendApplication.java
@@ -16,23 +16,6 @@ public class JmcBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(JmcBackendApplication.class, args);
 
-        JSONParser jsonParser = new JSONParser();
-        JSONObject jsonObject;
-        String jsonTest = "{\"id\": \"1\", \"name\":\"jjang-ggae\", \"thumbnail\":\"jpg\", \"score\":\"4\", \"location\":\"chungsuro\"}";
-
-        try {
-            jsonObject = (JSONObject) jsonParser.parse(jsonTest);
-        } catch(ParseException e){
-            throw new RuntimeException(e);
-        }
-
-        Map<String, Object> resultMap = new HashMap<>();
-
-        System.out.println("id값 파싱결과" + jsonObject.get("id"));
-        System.out.println("id값 파싱결과" + jsonObject.get("name"));
-
-//
-
     }
 
 }
