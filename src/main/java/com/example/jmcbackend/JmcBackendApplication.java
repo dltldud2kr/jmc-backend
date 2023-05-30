fab85375aeca8ebababcff2d633332500a0812fa
explorer-cat <sqlstyle@naver.com>
2023-05-31 01:40:09 +0900

    test

--- src/main/java/com/example/jmcbackend/JmcBackendApplication.java
+++ src/main/java/com/example/jmcbackend/JmcBackendApplication.java
@@ -3,7 +3,7 @@ package com.example.jmcbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//ddddㅇㄹㅇㄹ
+//ddddㅇㄹㅇㄹdfdf
 @SpringBootApplication
 public class JmcBackendApplication {
 
